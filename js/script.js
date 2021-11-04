be78bde10a79242439d48186cb798f38b7330dd3
Rapkowsky <robertrapkowski19@gmail.com>
2021-11-04 17:28:37 +0100

    to-do list skeleton added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
+const tasks = [
+	{
+		content: "utworzyć zadanie",
+		done: false,
+	},
+	{
+		content: "utworzono zadanie",
+		done: true,
+	},
+];
 
+const render = () => {
+	let htmlContent = "";
+
+	for (const task of tasks) {
+		htmlContent += `
+            <li${task.done ? ' style="text-decoration:line-through"' : ""}>
+            ${task.content}
+            </li>
+            `;
+	}
+	document.querySelector(".js-tasks").innerHTML = htmlContent;
+};
+
+const init = () => {
+	render();
+
+	const form = document.querySelector(".js-form");
+	form.addEventListener("submit", (event) => {
+		event.preventDefault();
+
+		const newTaskContent = document.querySelector(".js-newTask").value.trim();
+		if (newTaskContent === "") {
+			return;
+		}
+		tasks.push({
+			content: newTaskContent,
+		});
+		render();
+	});
+};
+
+init();
