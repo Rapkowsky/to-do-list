49dc69510bc60335434a18f30ab00090f30f0a68
Rapkowsky <robertrapkowski19@gmail.com>
2021-11-08 19:09:30 +0100

    adding new tasks and showing id a task is done

--- js/script.js
+++ js/script.js
@@ -15,17 +15,36 @@
 
 		for (const ticket of tasks) {
 			htmlText += `
-			<li>
+			<li ${ticket.done ? 'style="text-decoration: line-through"' : ""}>
 				${ticket.content}
 			</li>
 			`;
 		}
 		document.querySelector(".js-tasks").innerHTML = htmlText;
 	};
-	
+
+	const addNewTask = (newTicketContent) => {
+		tasks.push({
+			content: newTicketContent,
+		});
+		render();
+	};
+
+	const onFormSubmit = (event) => {
+		event.preventDefault();
+
+		const newTicketContent = document.querySelector(".js-newTask").value.trim();
+		if (newTicketContent === "") {
+			return;
+		}
+		addNewTask(newTicketContent);
+	};
 
 	const init = () => {
 		render();
+		const form = document.querySelector(".js-form");
+
+		form.addEventListener("submit", onFormSubmit);
 	};
 
 	init();
